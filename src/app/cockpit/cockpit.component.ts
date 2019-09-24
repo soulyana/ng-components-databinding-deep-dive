9d52dddefc4031fa6230ab36a5a9e43783a05404
Soulyana <soulyana.lakew@accenturefederal.com>
2019-09-24 13:32:38 -0400

    splitted apps into components 2

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+  newServerName = '';
+  newServerContent = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
